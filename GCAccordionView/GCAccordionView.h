ef89d579c8435ed3198cf0792a5aa659cfde9132
Gennady Chukin <gennady.chukin@gmail.com>
2013-04-02 11:14:11 +0400

    Initial

---
+++ GCAccordionView/GCAccordionView.h
@@ -0,0 +1,13 @@
+//
+//  GCAccordionView.h
+//  GCAccordion
+//
+//  Created by Gennady Chukin on 02.04.13.
+//  Copyright (c) 2013 Gennady Chukin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCAccordionView : UITableView
+
+@end
