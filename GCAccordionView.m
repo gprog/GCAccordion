ed7106cc02e78d176a1562edfeb7d762c58b2f9b
Gennady Chukin <gennady.chukin@gmail.com>
2013-04-02 12:09:18 +0400

    Initial

---
+++ GCAccordionView.m
@@ -0,0 +1,31 @@
+//
+//  GCAccordionView.m
+//  GCAccordion
+//
+//  Created by Gennady Chukin on 02.04.13.
+//  Copyright (c) 2013 Gennady Chukin. All rights reserved.
+//
+
+#import "GCAccordionView.h"
+
+@implementation GCAccordionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
