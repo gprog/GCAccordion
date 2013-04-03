82ea53bc193eb9f93049106ce745f1d0fa1867ae
Gennady Chukin <gennady.chukin@gmail.com>
2013-04-03 11:31:31 +0400

    0.1

--- GCAccordionView.h
+++ GCAccordionView.h
@@ -8,6 +8,115 @@
 
 #import <UIKit/UIKit.h>
 
-@interface GCAccordionView : UITableView
+@class GCAccordionView;
+
+@class GCAccordionHeaderView;
+
+@protocol GCAccordionHeaderViewDelegate <NSObject>
+
+- (void) didTouchAcordionHeaderView:(GCAccordionHeaderView *) headerView inSection: (NSInteger) section;
+
+@end
+
+@interface GCAccordionHeaderView : UIView {
+    UIImageView *disclosureIndicatorView;
+}
+
+@property (nonatomic, readonly) NSInteger section;
+-(id)initWithFrame:(CGRect)frame title:(NSString*)title section:(NSInteger)section delegate:(id <GCAccordionHeaderViewDelegate>)delegate;
+@property (nonatomic, weak) id <GCAccordionHeaderViewDelegate> delegate;
+
+@property (nonatomic) BOOL opened;
+
+@property (nonatomic, strong) UIImage *disclosureIndicator;
+@property (nonatomic) CGAffineTransform openTransform;
+
+@end
+
+
+@protocol GCAccordionViewDelegate <NSObject, UITableViewDelegate>
+
+//Configuring Rows for the Table View
+/*– tableView:heightForRowAtIndexPath:
+– tableView:indentationLevelForRowAtIndexPath:
+– tableView:willDisplayCell:forRowAtIndexPath:
+ */
+//Managing Accessory Views
+/*– tableView:accessoryButtonTappedForRowWithIndexPath:
+– tableView:accessoryTypeForRowWithIndexPath: Deprecated in iOS 3.0
+ */
+//Managing Selections
+/*– tableView:willSelectRowAtIndexPath:
+– tableView:didSelectRowAtIndexPath:
+– tableView:willDeselectRowAtIndexPath:
+– tableView:didDeselectRowAtIndexPath:
+ */
+//Modifying the Header and Footer of Sections
+- (CGFloat) accordionView:(GCAccordionView *)accordionView heightForHeaderInSection:(NSInteger)section;
+- (GCAccordionHeaderView *) accordionView:(GCAccordionView *)accordionView viewForHeaderInSection:(NSInteger)section;
+/*– tableView:viewForHeaderInSection:
+– tableView:viewForFooterInSection:
+
+– tableView:heightForFooterInSection:
+– tableView:willDisplayHeaderView:forSection:
+– tableView:willDisplayFooterView:forSection:
+ */
+//Editing Table Rows
+/*– tableView:willBeginEditingRowAtIndexPath:
+– tableView:didEndEditingRowAtIndexPath:
+– tableView:editingStyleForRowAtIndexPath:
+– tableView:titleForDeleteConfirmationButtonForRowAtIndexPath:
+– tableView:shouldIndentWhileEditingRowAtIndexPath:
+ */
+//Reordering Table Rows
+/*– tableView:targetIndexPathForMoveFromRowAtIndexPath:toProposedIndexPath:*/
+//Tracking the Removal of Views
+/*– tableView:didEndDisplayingCell:forRowAtIndexPath:
+– tableView:didEndDisplayingHeaderView:forSection:
+– tableView:didEndDisplayingFooterView:forSection:*/
+//Copying and Pasting Row Content
+/*– tableView:shouldShowMenuForRowAtIndexPath:
+– tableView:canPerformAction:forRowAtIndexPath:withSender:
+– tableView:performAction:forRowAtIndexPath:withSender:*/
+//Managing Table View Highlighting
+/*– tableView:shouldHighlightRowAtIndexPath:
+– tableView:didHighlightRowAtIndexPath:
+– tableView:didUnhighlightRowAtIndexPath:*/
+@end
+
+@protocol GCAccordionViewDataSource <NSObject>
+
+- (NSInteger) numberOfSectionsInAccordionView: (GCAccordionView *) accordionView;
+
+- (NSInteger) accordionView:(GCAccordionView *)accordionView numberOfRowsInSection:(NSInteger)section;
+
+- (UITableViewCell *) accordionView: (GCAccordionView *) accordionView cellForRowAtIndexPath: (NSIndexPath *) indexPath;
+
+- (NSString *) accordionView: (GCAccordionView *) accordionView titleForHeaderInSection: (NSInteger) section;
+
+
+/////////////////////
+- (BOOL) accordionView: (GCAccordionView *) accordionView canExpandColapseSection: (NSInteger) section;
+
+//Inserting or Deleting Table Rows
+
+- (void) accordionView: (GCAccordionView *) accordionView commitEditingStyle: (UITableViewStyle *) style forRowAtIndexPath: (NSIndexPath *) indexPath;
+- (BOOL) accordionView: (GCAccordionView *) accordionView canEditRowAtIndexPath: (NSIndexPath *) indexPath;
+
+//Reordering Table Rows
+//– tableView:canMoveRowAtIndexPath:
+//– tableView:moveRowAtIndexPath:toIndexPath:
+
+
+@end
+
+@interface GCAccordionView : UITableView<UITableViewDataSource, UITableViewDelegate, GCAccordionHeaderViewDelegate>
+
+
+@property (nonatomic, assign) IBOutlet id <GCAccordionViewDataSource> accordionDataSource;
+@property (nonatomic, assign) IBOutlet id <GCAccordionViewDelegate> accordionDelegate;
+@property (nonatomic) BOOL multipleExpandable;
+
+- (UITableViewCell *) cellWithIdentifier: (NSString *) identifier;
 
 @end
--- GCAccordionView.m
+++ GCAccordionView.m
@@ -8,24 +8,281 @@
 
 #import "GCAccordionView.h"
 
+@implementation GCAccordionHeaderView
+
+- (void) didTouch
+{
+    if (self.delegate && [self.delegate respondsToSelector:@selector(didTouchAcordionHeaderView:inSection:)])  [self.delegate didTouchAcordionHeaderView:self inSection:self.section];
+}
+
+- (void) setOpened:(BOOL)opened
+{
+    if (_opened != opened)
+    {
+        _opened = opened;
+        [UIView animateWithDuration:0.2 animations:^{
+            if (_opened && disclosureIndicatorView)
+                disclosureIndicatorView.transform = CGAffineTransformMakeRotation(M_PI_2);
+            else
+                disclosureIndicatorView.transform = CGAffineTransformMakeRotation(0);
+        }];
+    }
+}
+
+- (void) setDisclosureIndicator:(UIImage *)disclosureIndicator
+{
+    if (_disclosureIndicator != disclosureIndicator)
+    {
+        _disclosureIndicator = disclosureIndicator;
+        if (!disclosureIndicatorView)
+        {
+            disclosureIndicatorView = [[UIImageView alloc] initWithImage:_disclosureIndicator];
+            [self addSubview:disclosureIndicatorView];
+            CGRect diframe = disclosureIndicatorView.frame;
+            diframe.origin.x = 12;
+            diframe.origin.y = self.frame.size.height/2-diframe.size.height/2;
+            disclosureIndicatorView.frame = diframe;
+        }
+    }
+}
+
+- (void) setupOpenClose:(NSNotification *) notification
+{
+    if ([[notification.userInfo objectForKey:@"section"] isEqualToString:[NSString stringWithFormat:@"%d",self.section]])
+    {
+        self.opened = [[notification.userInfo objectForKey:@"opened"] boolValue];
+    }
+}
+
+- (id) initWithFrame:(CGRect)frame title:(NSString *)title section:(NSInteger)section delegate:(id<GCAccordionHeaderViewDelegate>)delegate
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        self.backgroundColor = [UIColor colorWithRed:0.427 green:0.435 blue:0.459 alpha:1];
+        UILabel *label = [[UILabel alloc] initWithFrame:self.frame];
+        label.backgroundColor= [UIColor clearColor];
+        label.textColor = [UIColor whiteColor];
+        label.font = [UIFont boldSystemFontOfSize:17.0];
+        label.text = title;
+        label.textAlignment = NSTextAlignmentCenter;
+        _section = section;
+        self.delegate = delegate;
+        [self addSubview:label];
+        label.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+        UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTouch)];
+        [self addGestureRecognizer:tapGesture];
+        _delegate = delegate;
+        self.userInteractionEnabled = YES;
+        self.opened = NO;
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(setupOpenClose:) name:@"DidOpenCloseSection" object:nil];
+    }
+    return  self;
+}
+
+- (void) dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+@end
+
+
+@interface GCAccordionView ()
+
+@property (nonatomic, strong, readonly) NSMutableArray *expandedSections;
+
+
+@end
+
+
 @implementation GCAccordionView
+
+
+- (void) setup
+{
+    self.delegate = self;
+    self.dataSource = self;
+    self.multipleExpandable = YES;
+}
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
+    if (self)
+    {
+        [self setup];
     }
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
+- (id) init
 {
-    // Drawing code
+    self  = [super init];
+    if (self)
+    {
+        [self setup];
+    }
+    return self;
 }
-*/
+
+- (id) initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self)
+    {
+        [self setup];
+    }
+    return self;
+}
+
+- (id) initWithFrame:(CGRect)frame style:(UITableViewStyle)style
+{
+    self = [super initWithFrame:frame style:style];
+    if (self)
+    {
+        [self setup];
+    }
+    return self;
+}
+
+- (void) setDataSource:(id<UITableViewDataSource>)dataSource
+{
+    if (dataSource == self) [super setDataSource:dataSource];
+}
+
+- (void) setDelegate:(id<UITableViewDelegate>)delegate
+{
+    if (delegate == self) [super setDelegate:delegate];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
+{
+    if (self.accordionDataSource) return [self.accordionDataSource numberOfSectionsInAccordionView:self]; else return 0;
+}
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (self.accordionDataSource)
+    {
+        if (![_expandedSections containsObject: [NSString stringWithFormat:@"%d", section]]) return  0;
+        return [self.accordionDataSource accordionView:self numberOfRowsInSection:section];
+    }else return 0;
+}
+
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (self.accordionDataSource) return [self.accordionDataSource accordionView:self cellForRowAtIndexPath:indexPath]; else return nil;
+}
+
+- (NSString *) tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    if (self.accordionDataSource)
+    {
+        NSString * title;
+        if ([self.accordionDataSource respondsToSelector:@selector(accordionView:titleForHeaderInSection:)]) title = [self.accordionDataSource accordionView:self titleForHeaderInSection:section];
+        if (!title) title = [NSString stringWithFormat:@"Section %d", section];
+        return title;
+    } else return nil;
+}
+
+
+#pragma mark - UITableViewDelegate
+
+- (CGFloat) tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    if (self.accordionDelegate && [self.accordionDelegate respondsToSelector:@selector(accordionView:heightForHeaderInSection:)]) return [self.accordionDelegate accordionView:self heightForHeaderInSection:section]; else return 50;
+}
+
+- (UIView *) tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    GCAccordionHeaderView *headerView;
+    if (self.accordionDelegate && [self.accordionDelegate respondsToSelector:@selector(accordionView:viewForHeaderInSection:)])
+    {
+        headerView = [self.accordionDelegate accordionView:self viewForHeaderInSection:section];
+    } else {
+        NSString *title = ([self.accordionDataSource respondsToSelector:@selector(accordionView:titleForHeaderInSection:)]) ? [self.accordionDataSource accordionView:self titleForHeaderInSection:section] : [self tableView:tableView titleForHeaderInSection:section];
+        headerView = [[GCAccordionHeaderView alloc] initWithFrame:CGRectMake(0, 0, tableView.bounds.size.width, 50) title:title section:section delegate:self];
+    }
+    headerView.disclosureIndicator = [UIImage imageNamed:@"arrow.png"];
+    headerView.opened = [self isExpandedSection:section];
+    return headerView;
+}
+
+#pragma  mark - Expand Collapse Logic
+
+- (BOOL) isExpandedSection: (NSInteger) section
+{
+    if (!_expandedSections) _expandedSections = [[NSMutableArray alloc] init];
+    return [_expandedSections containsObject:[NSString stringWithFormat:@"%d", section]];
+}
+
+
+- (void) expandSection: (NSInteger) section
+{
+    NSMutableArray *indexPathsToInsert = [[NSMutableArray alloc] init];
+    if (![self isExpandedSection:section])
+    {
+        NSDictionary *userInfo = [NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithFormat:@"%d",section], @"section", [NSNumber numberWithBool:YES], @"opened", nil];
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"DidOpenCloseSection" object:nil userInfo:userInfo];
+        if (!self.multipleExpandable)
+        {
+            for (NSString *sectionToCollapse in _expandedSections) {
+                [self collapseSection:[sectionToCollapse integerValue]];
+            }
+        }
+        [_expandedSections addObject:[NSString stringWithFormat:@"%d", section]];
+        for (NSInteger i = 0; i<[self tableView:self numberOfRowsInSection:section]; i++) {
+            [indexPathsToInsert addObject:[NSIndexPath indexPathForRow:i inSection:section]];
+        }
+        [self beginUpdates];
+        [self insertRowsAtIndexPaths:indexPathsToInsert withRowAnimation:UITableViewRowAnimationTop];
+        [self endUpdates];
+    }
+}
+
+- (void) collapseSection: (NSInteger) section
+{
+    if ([self isExpandedSection:section])
+    {
+        NSDictionary *userInfo = [NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithFormat:@"%d",section], @"section", [NSNumber numberWithBool:NO], @"opened", nil];
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"DidOpenCloseSection" object:nil userInfo:userInfo];
+        NSMutableArray *indexPathsToDelete = [[NSMutableArray alloc] init];
+        //Collapse logic
+        for (NSInteger i = 0; i<[self tableView:self numberOfRowsInSection:section]; i++) {
+            [indexPathsToDelete addObject:[NSIndexPath indexPathForRow:i inSection:section]];
+        }
+        [_expandedSections removeObject:[NSString stringWithFormat:@"%d", section]];
+        [self beginUpdates];
+        [self deleteRowsAtIndexPaths:indexPathsToDelete withRowAnimation:UITableViewRowAnimationTop];
+        [self endUpdates];
+    }
+}
+
+- (void) toggleSection: (NSInteger) section
+{
+    if (self.accordionDataSource && [self.accordionDataSource respondsToSelector:@selector(accordionView:canExpandColapseSection:)] && [self.accordionDataSource accordionView:self canExpandColapseSection:section])
+    {
+        if ([self isExpandedSection:section]) [self collapseSection:section]; else [self expandSection:section];
+    }
+}
+
+#pragma mark - GCAccordionHeaderViewDelegate
+
+- (void) didTouchAcordionHeaderView:(GCAccordionHeaderView *)headerView inSection:(NSInteger)section
+{
+    [self toggleSection:section];
+}
+
+
+#pragma mark - helpers
+- (UITableViewCell *) cellWithIdentifier:(NSString *)identifier
+{
+    UITableViewCell *cell = [self dequeueReusableCellWithIdentifier:identifier];
+    if (!cell) cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    return cell;
+}
 
 @end
